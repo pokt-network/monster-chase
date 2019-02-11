f07f24445dedd752cd039e6330ff73f5b8278791
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2019-02-10 23:44:15 -0400

    Added how to play

--- monster-chase/monster-chase/ViewControllers/Help/HowToPlayViewController.swift
+++ monster-chase/monster-chase/ViewControllers/Help/HowToPlayViewController.swift
@@ -10,5 +10,25 @@ import Foundation
 import UIKit
 
 class HowToPlayViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    @IBAction func menuBtnTapped(_ sender: Any) {
+        if let container = self.so_containerViewController {
+            container.isSideViewControllerPresented = true
+        }
+    }
 }
