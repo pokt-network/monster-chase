4c34d8850f8a6a6626e0b6e8b2e9bb06d004094a
Luis de Leon <luyzdeleon@gmail.com>
2019-03-15 10:30:49 -0400

    Aligned help texts

--- monster-chase/monster-chase/Views/BackgroundView.swift
+++ monster-chase/monster-chase/Views/BackgroundView.swift
@@ -30,7 +30,8 @@ class BackgroundView: UIView {
         contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         contentView.layer.borderWidth = 5
         contentView.layer.borderColor = AppColors.base.cgColor()
-        contentView.layer.cornerRadius = contentView.frame.height * 0.05
+        contentView.layer.cornerRadius = 8
+        //contentView.layer.cornerRadius = contentView.frame.height * 0.05
         
         // Remove user interaction
         contentView.isUserInteractionEnabled = false
